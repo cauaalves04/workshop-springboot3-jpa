3f729126927842d43882006bc92251180cd19ac0
Caua Alves <zcauanzim26@gmail.com>
2025-11-19 10:28:22 -0300

    User update

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -17,7 +17,7 @@ public class User implements Serializable {
     private Long id;
     private String name;
     private String email;
-    private String fone;
+    private String phone;
     private String password;
 
     @JsonIgnore
@@ -27,12 +27,12 @@ public class User implements Serializable {
     public User() {
     }
 
-    public User(Long id, String name, String email, String fone, String password) {
+    public User(Long id, String name, String email, String phone, String password) {
         super();
         this.id = id;
         this.name = name;
         this.email = email;
-        this.fone = fone;
+        this.phone = phone;
         this.password = password;
     }
 
@@ -44,12 +44,12 @@ public class User implements Serializable {
         this.email = email;
     }
 
-    public String getFone() {
-        return fone;
+    public String getPhone() {
+        return phone;
     }
 
-    public void setFone(String fone) {
-        this.fone = fone;
+    public void setPhone(String fone) {
+        this.phone = fone;
     }
 
     public Long getId() {
